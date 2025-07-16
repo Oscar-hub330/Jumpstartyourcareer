63ad42cb7c8112baa0a135af235a28daef9ad50c
Tshepiso Perseverance Makuoa <164529937+SnowwhiteA1@users.noreply.github.com>
2025-07-16 10:38:53 +0200

    final commits, database works well, it can retrieve from postman, few touches left

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss';
+    
 
 export default defineConfig({
   plugins: [react({
     jsxImportSource: '@emotion/react',
     babel: {
       plugins: ['@emotion/babel-plugin'],
+      
     },
   })],
 })
